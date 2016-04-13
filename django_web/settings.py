ce5e1467a0e6c01567fea2f6571e3e3f9e5fc5c1
linchao <linchao@jaxus.com>
2016-04-13 20:11:23 +0800

    set debug=False

--- django_web/settings.py
+++ django_web/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '_6fei600u*argg@ui6yuv)-o1g7_(1xpf-4nljuv5fztb8%j!t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost','www.superlin.cc','kantu.superlin.cc','www.kantujun.com','www.yz5a.com']
 
 
 # Application definition
